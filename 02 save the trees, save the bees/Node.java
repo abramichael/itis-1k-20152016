3d002296109c3e5c4bbac07ad53232a350133f1c
ma <ma@fosslabs.ru>
2016-04-03 22:22:09 +0300

    tree creation and traverse examples added

---
+++ 02 save the trees, save the bees/Node.java
@@ -0,0 +1,31 @@
+/**
+ * Created by ma on 30.03.2016.
+ */
+public class Node {
+    private int value;
+    private Node left, right;
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+}
---
+++ 02 save the trees, save the bees/Tree.java
@@ -0,0 +1,90 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+/**
+ * Created by ma on 30.03.2016.
+ */
+public class Tree {
+    private Node root;
+
+    public Tree(int n) {
+        root = createTree(n);
+    }
+
+    private static Node createTree(int n) {
+        if (n == 0)
+            return null;
+
+        Node r = new Node();
+        r.setValue(n);
+        int nl = n / 2;
+        int nr = n - nl - 1;
+        r.setLeft(createTree(nl));
+        r.setRight(createTree(nr));
+        return r;
+    }
+    public static void printTree(int n, Node p) {
+        if (p != null) {
+            printTree(n + 1, p.getRight());
+            for (int i = 0; i < n; i++) {
+                System.out.print("  ");
+            }
+            System.out.println(p.getValue());
+            printTree(n + 1, p.getLeft());
+        }
+    }
+
+    public void print() {
+        printTree(0, root);
+    }
+
+    public static int n = 0;
+    public static void enumerateDepthKLP(Node p) {
+        if (p != null) {
+            p.setValue(n++);
+            enumerateDepthKLP(p.getLeft());
+            enumerateDepthKLP(p.getRight());
+        }
+    }
+
+    public static void main(String[] args) {
+        Tree t = new Tree(15);
+        //enumerateDepthKLP(t.getRoot());
+        //t.enumerateBreadthLP();
+        t.enumerateLP();
+        t.print();
+    }
+
+    public void enumerateBreadthLP() {
+        Queue<Node> q = new LinkedList<>();
+        q.offer(root);
+        int n = 0;
+        while (!q.isEmpty()) {
+            Node p = q.poll();
+            p.setValue(n++);
+            if (p.getLeft()!= null)
+                q.offer(p.getLeft());
+            if (p.getRight()!= null)
+                q.offer(p.getRight());
+        }
+    }
+    public void enumerateLP() {
+        Stack<Node> st = new Stack<>();
+        st.push(root);
+        int n = 0;
+        while (!st.isEmpty()) {
+            Node p = st.pop();
+            p.setValue(n++);
+            if (p.getLeft()!= null)
+                st.push(p.getLeft());
+            if (p.getRight()!= null)
+                st.push(p.getRight());
+        }
+    }
+
+
+    public Node getRoot() {
+        return root;
+    }
+}
